4d5dcdf09a8d462e16826815376d020166829f71
Jules <1658895307@qq.com>
2019-11-18 00:58:56 +0800

    Add micro server

--- user/src/main/java/com/remo/UserApplication.java
+++ user/src/main/java/com/remo/UserApplication.java
@@ -3,11 +3,13 @@ package com.remo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.remo.mapper")
 @EnableSwagger2
+@EnableEurekaClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
