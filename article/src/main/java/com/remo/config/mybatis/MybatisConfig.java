1713735b0db82369eb492348e544fd593d46a14e
Jules <1658895307@qq.com>
2019-12-12 17:17:48 +0800

    some changes

--- article/src/main/java/com/remo/config/mybatis/MybatisConfig.java
+++ article/src/main/java/com/remo/config/mybatis/MybatisConfig.java
@@ -34,9 +34,9 @@ public class MybatisConfig {
         return new OptimisticLockerInterceptor();
     }
 
-    /*
+    /**
      * 分页插件，自动识别数据库类型
-     * 多租户，请参考官网【插件扩展】
+     *
      */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
