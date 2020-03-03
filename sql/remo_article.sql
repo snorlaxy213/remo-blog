476423666785a4607f04402b79a4cee2e63dca19
Vino <1658895307@qq.com>
2020-03-04 02:10:30 +0800

    update last db file
    Signed-off-by: Vino <1658895307@qq.com>

--- sql/remo_article.sql
+++ sql/remo_article.sql
@@ -1,88 +1,83 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : remo
-Source Server Version : 80018
-Source Host           : localhost:3306
-Source Database       : remo_article
+ Source Server         : vino
+ Source Server Type    : MySQL
+ Source Server Version : 80013
+ Source Host           : localhost:3306
+ Source Schema         : remo_article
 
-Target Server Type    : MYSQL
-Target Server Version : 80018
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80013
+ File Encoding         : 65001
 
-Date: 2020-01-10 14:39:08
+ Date: 04/03/2020 02:09:55
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for article
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
-CREATE TABLE `article` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `articleId` bigint(20) NOT NULL COMMENT '文章id',
-  `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '作者',
-  `originalAuthor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文章原作者',
-  `articleTitle` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章标题',
-  `articleContent` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章内容',
-  `articleTags` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文章标签',
-  `articleType` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章类型',
-  `articleCategories` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文章分类',
-  `publishDate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '发布文章日期',
-  `updateDate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '更新文章日期',
-  `articleUrl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章url',
-  `articleTabloid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章摘要',
-  `likes` int(11) DEFAULT NULL COMMENT '文章喜欢数',
-  `lastArticleId` bigint(20) DEFAULT NULL COMMENT '上一篇文章id',
-  `nextArticleId` bigint(20) DEFAULT NULL COMMENT '下一篇文章id',
-  `createTime` datetime(6) DEFAULT NULL COMMENT '创建时间',
-  `createUser` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '创建人',
-  `updateTime` datetime(6) DEFAULT NULL COMMENT '更新时间',
-  `updateUser` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '更新人',
-  `version` int(11) DEFAULT NULL COMMENT '乐观锁',
+CREATE TABLE `article`  (
+  `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `article_id` bigint(20) NOT NULL COMMENT '文章id',
+  `author_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '作者',
+  `original_author_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '文章原作者',
+  `article_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章标题',
+  `article_content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章内容',
+  `article_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章类型',
+  `article_url` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章url',
+  `article_tabloid` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章摘要',
+  `likes` int(6) NOT NULL COMMENT '文章喜欢数',
+  `last_article_id` bigint(20) NULL DEFAULT NULL COMMENT '上一篇文章id',
+  `next_article_id` bigint(20) NULL DEFAULT NULL COMMENT '下一篇文章id',
+  `create_time` datetime(6) NOT NULL COMMENT '创建时间',
+  `create_user` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '创建人',
+  `update_time` datetime(6) NULL DEFAULT NULL COMMENT '更新时间',
+  `update_user` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '更新人',
+  `is_delete` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '1-已删除;0-存在',
+  `version` int(11) NULL DEFAULT NULL COMMENT '乐观锁',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of article
 -- ----------------------------
-INSERT INTO `article` VALUES ('1', '0', 'vino', 'nono', 'string', '测试', 'vino', 'string', 'string', 'string', 'string', 'string', 'string', '0', '0', '0', '2019-08-28 23:16:47.091000', 'vino', '2019-08-29 00:37:33.868000', 'vino', '0');
-INSERT INTO `article` VALUES ('2', '0', 'string', 'string', 'string', 'string', 'string', 'string', 'string', '2019-12', 'string', 'string', 'string', '0', '0', '0', '2019-12-12 11:22:02.220000', 'vino', null, null, '0');
+INSERT INTO `article` VALUES (1, 0, 'vino', 'nono', 'string', '测试', 'string', 'string', 'string', 0, 0, 0, '2019-08-28 23:16:47.091000', 'vino', '2019-08-29 00:37:33.868000', 'vino', '', 0);
 
 -- ----------------------------
--- Table structure for article_tag
+-- Table structure for relation_article_tag
 -- ----------------------------
-DROP TABLE IF EXISTS `article_tag`;
-CREATE TABLE `article_tag` (
+DROP TABLE IF EXISTS `relation_article_tag`;
+CREATE TABLE `relation_article_tag`  (
   `articleId` int(11) NOT NULL,
   `tagId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- ----------------------------
--- Records of article_tag
--- ----------------------------
-INSERT INTO `article_tag` VALUES ('0', '2');
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for tag
 -- ----------------------------
 DROP TABLE IF EXISTS `tag`;
-CREATE TABLE `tag` (
-  `tagId` int(10) NOT NULL AUTO_INCREMENT,
-  `tagDesc` varchar(100) DEFAULT NULL,
-  `tagType` char(1) NOT NULL DEFAULT 'F' COMMENT 'F - Field\nC - Category',
-  `parentId` int(10) DEFAULT NULL,
-  `orderSeq` int(5) DEFAULT NULL,
-  `createTime` datetime(6) DEFAULT NULL COMMENT '创建时间',
-  `createUser` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '创建人',
-  `updateTime` datetime(6) DEFAULT NULL COMMENT '更新时间',
-  `updateUser` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '更新人',
-  `version` int(11) DEFAULT NULL COMMENT '乐观锁',
-  PRIMARY KEY (`tagId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `tag`  (
+  `tag_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `tag_description` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '简介',
+  `order_num` tinyint(3) NOT NULL COMMENT '序号',
+  `create_tiem` datetime(6) NOT NULL COMMENT '创建时间',
+  `create_user` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '创建人',
+  `update_time` datetime(6) NULL DEFAULT NULL COMMENT '更新时间',
+  `update_user` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '更新人',
+  `is_delete` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '1-已删除;0-存在',
+  `version` int(11) NULL DEFAULT NULL COMMENT '乐观锁',
+  PRIMARY KEY (`tag_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tag
 -- ----------------------------
-INSERT INTO `tag` VALUES ('2', '生活', 'F', null, '2', '2019-12-11 14:25:11.119000', 'vino', null, null, null);
+INSERT INTO `tag` VALUES (1, '技术', 1, '2019-12-11 12:03:57.593000', 'vino', NULL, NULL, '0', NULL);
+INSERT INTO `tag` VALUES (2, '生活', 2, '2019-12-11 14:25:11.119000', 'vino', NULL, NULL, '0', NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
