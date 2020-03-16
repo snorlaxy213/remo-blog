368035e400c68fa688ef9ad0d36af71d2a7cdbda
Jules <1658895307@qq.com>
2020-03-16 18:31:10 +0800

    develop seckill function

--- seckill/src/main/java/com/remo/common/PrefixConstant.java
+++ seckill/src/main/java/com/remo/common/PrefixConstant.java
@@ -5,5 +5,7 @@ public class PrefixConstant {
     // verifyCode缓存前缀
     public static final String SECKILL_VERIFYCODE_PREFIX = "seckill.verifyCode.";
     // seckillPath
-    public static final String SECKILL_SECKILLPATH_PREFIX = "seckill.seckillPath.";
+    public static final String SECKILL_SECKILL_PATH_PREFIX = "seckill.seckillPath.";
+    // seckillOrder
+    public static final String SECKILL_SECKILL_ORDER_PREFIX = "seckill.seckillOrder.";
 }
--- seckill/src/main/java/com/remo/controller/SeckillController.java
+++ seckill/src/main/java/com/remo/controller/SeckillController.java
@@ -1,6 +1,8 @@
 package com.remo.controller;
 
+import com.remo.pojo.dto.SeckillOrderDto;
 import com.remo.pojo.vo.ResponseVo;
+import com.remo.service.SeckillOrderService;
 import com.remo.service.SeckillService;
 import com.remo.utils.ImageUtil;
 import com.remo.utils.ResponseUtil;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
 
 @Slf4j
 @RestController
@@ -24,6 +27,11 @@ public class SeckillController {
 
     @Resource(name = "seckillServiceImpl")
     SeckillService seckillServiceImpl;
+
+    @Resource(name = "seckillOrderServiceImpl")
+    SeckillOrderService seckillOrderServiceImpl;
+
+    private HashMap<Long, Boolean> localOverMap = new HashMap<>();
 
     @RequestMapping(value = "/path", method = RequestMethod.GET)
     public ResponseVo getSeckillPath(@RequestParam("userId") Long userId,
@@ -44,7 +52,7 @@ public class SeckillController {
     }
 
     @RequestMapping(value = "/{path}/seckill", method = RequestMethod.POST)
-    public ResponseVo miaosha(@PathVariable("path") String path,
+    public ResponseVo seckill(@PathVariable("path") String path,
                               @RequestParam("userId") Long userId,
                               @RequestParam("goodsId") long goodsId) {
         //验证path
@@ -54,16 +62,14 @@ public class SeckillController {
         }
 
         //是否已经秒杀到
-        MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(Long.valueOf(user.getNickname()), goodsId);
-        if (order != null) {
-            result.withError(REPEATE_MIAOSHA.getCode(), REPEATE_MIAOSHA.getMessage());
-            return result;
+        SeckillOrderDto seckillOrderDto = seckillOrderServiceImpl.getSeckillOrderByUserIdAndGoodsId(userId, goodsId);
+        if (seckillOrderDto != null) {
+            return ResponseUtil.initErrorResultVO("请勿重复购买");
         }
         //内存标记，减少redis访问
         boolean over = localOverMap.get(goodsId);
         if (over) {
-            result.withError(MIAO_SHA_OVER.getCode(), MIAO_SHA_OVER.getMessage());
-            return result;
+            return ResponseUtil.initErrorResultVO("物品已下架");
         }
         //预见库存
         Long stock = redisService.decr(GoodsKey.getMiaoshaGoodsStock, "" + goodsId);
