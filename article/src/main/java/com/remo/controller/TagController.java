ee3d31bad4b37b4c4ed78a9d0160990017ee885b
Vino <1658895307@qq.com>
2020-04-05 13:19:05 +0800

    improve

--- article/src/main/java/com/remo/controller/TagController.java
+++ article/src/main/java/com/remo/controller/TagController.java
@@ -11,12 +11,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
