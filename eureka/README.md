d39e820b170a713127d7b629942c1023854e6ee7
Vino <1658895307@qq.com>
2020-06-10 00:10:36 +0800

    update eureka info

---
+++ eureka/README.md
@@ -0,0 +1,9 @@
+
+### Eureka的自我保护模式
+默认情况下，如果eureka server在一定时间内（90s）没有接收到每个微服务实例的心跳，eureka server将会注销该实例。
+
+当网络分区故障发生时，微服务与eureka server之间无法正常通信，以上行为可能变得非常危险了，因为微服务本身其实是健康的，此时本不应该注销这个微服务。eureka通过“自我保护模式来解决这个问题”。
+
+在自我保护模式中，eureka server会保护服务注册表中的信息，不再注销任何服务实例。当它收到的心跳数重新恢复到阀值以上时，该eureka server节点就会自动退出自我保护模式。
+
+可以使用**eureka.serverenable-self-preservation=false**禁用自我保护模式。
--- eureka/src/main/java/com/remo/ServerApplication.java
+++ eureka/src/main/java/com/remo/ServerApplication.java
@@ -3,11 +3,23 @@ package com.remo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @SpringBootApplication
 @EnableEurekaServer //标识为Eureka Server,接受其他微服务注册进来
-public class ServerApplication {
+public class ServerApplication extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Configure HttpSecurity as needed (e.g. enable http basic).
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 }
