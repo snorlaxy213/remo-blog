cc5583a4ce3f30a9f854e9032f4a68cf39a209ff
Jules <iso2ez4vino@outlook.com>
2021-02-01 18:37:29 +0800

    Jules Update
    Signed-off-by: Jules <iso2ez4vino@outlook.com>

---
+++ gateway/src/main/java/com/remo/gateway/filter/IpBlackListFilter.java
@@ -0,0 +1,50 @@
+package com.remo.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.ipresolver.RemoteAddressResolver;
+import org.springframework.cloud.gateway.support.ipresolver.XForwardedRemoteAddressResolver;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.InetSocketAddress;
+
+@Component
+public class IpBlackListFilter implements GlobalFilter, Ordered {
+
+    public static final Logger log = LoggerFactory.getLogger(IpBlackListFilter.class);
+
+    private final RemoteAddressResolver remoteAddressResolver = XForwardedRemoteAddressResolver
+            .maxTrustedIndex(1);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        try {
+            InetSocketAddress remoteAddress = remoteAddressResolver.resolve(exchange);
+            String clientIp = remoteAddress.getHostName();
+            // redisTemplate.hasKey(BLACKLIST_IP_KEY + StringConsts.COLON + clientIp)
+            if (true) {
+                log.info("intercept invalid request from forbidden ip {}", clientIp);
+                exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                return Mono.empty();
+            }
+        } catch (Exception e) {
+            log.error("IpBlackListFilter error", e);
+        }
+        return chain.filter(exchange);
+    }
+
+    /**
+     * order的规则：order的值越小，优先级越高
+     * order如果不标注数字，默认最低优先级，因为其默认值是int最大值
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
