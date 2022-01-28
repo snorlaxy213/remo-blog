0d3d8c925849a740893a9f08fcf6bcab0aa5fd9d
Grio Vino <iso2ez4vino@outlook.com>
2022-01-29 04:14:54 +0800

    Remove the code that uses cloud

--- article/src/main/java/com/remo/article/ArticleApplication.java
+++ article/src/main/java/com/remo/article/ArticleApplication.java
@@ -3,16 +3,11 @@ package com.remo.article;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableEurekaClient
-@EnableFeignClients
-@EnableHystrix
 @MapperScan("com.remo.article.mapper")
 public class ArticleApplication {
     public static void main(String[] args) {
